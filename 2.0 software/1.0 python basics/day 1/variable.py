2c8c1cbb55bfe55413264a296428b3744b994055
Emmanuella <afamemmanuella@gmail.com>
2025-07-23 15:13:32 +0200

    have completed my variable lessons

---
+++ 2.0 software/1.0 python basics/day 1/variable.py
@@ -0,0 +1,56 @@
+print("hello deepseeds")
+
+#why do we need variables
+
+name = "blink"
+print("i am Ella , i love this name- Ella, was invented by me Ella, and i really love the name Ella")
+
+print("my name is" + name + "i love the name" + name + "was invented by me" + name + "and i love the name " + name + "so much")
+
+#numbrs
+whole_number = 42
+decimal_number = 3.6332
+complex_number = 2 + 3j
+
+#text
+
+greeting = "hello, World"
+single_char = "A"
+
+#booleans
+
+is_sunny = True
+is_raining = False
+
+#check variable type
+print(type(whole_number))
+print(type(greeting))
+print(type(1.33))
+
+#variables basic examples
+#1 concastination.
+first_name = "Emma"
+last_name = "bella"
+
+combine_name= first_name + last_name
+print("my full name is" + combine_name)
+
+
+#A ban name generator
+
+
+name=input("what is your name")
+
+age=input("what is your age")
+
+fav_meal=input("what is your favourite mile")
+school=input("what is th name of your school ")
+department=input("what is your department")
+best_friend=input("who is your best friend")
+
+print(f"your name is {name} and you are {age} years old. your fav food is {fav_meal} and your school is {school} and your best friend is {best_friend}")
+
+
+
+
+
