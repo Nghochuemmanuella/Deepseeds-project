c3666c359e8a3526aec83b769fe58e93436f4f7d
Emmanuella <afamemmanuella@gmail.com>
2025-07-24 01:51:17 +0200

    first example on elif

---
+++ 2.0 software/1.0 python basics/day 2/conditionals.py
@@ -0,0 +1,42 @@
+#vote eligability
+#age=int(input("Please enter your age"))
+#if age > 18:
+  #  print("Yes you can vote")
+#else:
+   # print("please wait for next election")
+    
+    
+    #exercise, take a persons age, check if > 15. if yes, they can play baskate ball team
+    
+  #  age=int(input("Please enter your age"))
+   # if age > 16:
+     #   print("Yes u can play in the basket ball team")
+  #  else:
+       # print("you must be above 16 to play")
+        
+        #if else elif chain
+        
+#command=input("enter 'exit' to end program and 'continue' to keep going")
+#if command=="exit":
+ #   print("exiting cmd")
+    
+#elif command=="continue":
+ #   print("continue enjoying...")
+    
+#else:
+ #   print("wrong command")
+    
+    
+    #multiple elif
+#print("_"*50)
+grade=float(input("what is your grade: "))
+if grade>=80:
+    print("A grade")
+elif grade>=70:
+    print("B+😍")
+elif grade>=60:
+    print("B grade")
+elif grade>=50:
+    print("C grade")
+else:
+    print("resit")
